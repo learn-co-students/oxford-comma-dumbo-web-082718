5026fd092730e51750a41665826e37f8cc42104c
Mendy <mendybeltcohen@gmail.com>
2018-08-13 15:47:28 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+  if array.length == 1
+     array.join
+  elsif array.length == 2
+     array.join(' and ') 
+  elsif array.length == 3
+      array[0..1].join(', ') << ', ' <<  'and ' << array[2]
+      elsif array.length > 3 
+      last = array.pop 
+      array.join(', ') << ', and ' << last
+  end
 end
