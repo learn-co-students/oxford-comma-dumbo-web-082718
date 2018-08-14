e3fd23a849d9f4cae1784d5a06faa9d887709d80
Gavin Soko <sokolofg@gmail.com>
2018-08-14 05:40:09 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
-def oxford_comma(array)
+  def oxford_comma(array)
+    if array.length == 2
+      array.insert(1, "and")
+      return array.join(" ")
 
-end
+    elsif array.length == 1
+    return array.join(" ")
+else 
+     newS = array.slice(-1)
+      array.delete_at(-1)
+  newGuy = newS.insert(0, ", and ")
+  newGuy.to_s
+  newArray = array.join(", ")
+  newArray + newGuy
+
+  end
+  
+  end
