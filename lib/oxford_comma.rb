0d0d946e3d14084103971779157561ac83fc0856
Jackie Ha <jha2@wellesley.edu>
2018-08-20 20:28:52 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
 def oxford_comma(array)
-
+  if array.size == 2 
+    array.join(" and ")
+  elsif array.size >= 3
+    array[-1] = "and " + array[-1]
+    array.join(", ")
+  else
+    array.join
+  end 
 end
+
+
+
+
+#def oxford_comma(array)
+ # if array.size == 1 
+   # return array.join
+  #elseif array.size == 2 
+   # return array.join("and")
+  #else
+   # return array[0..-2].join(", ") + ", and " + array[-1]
+  #end 
+# end
